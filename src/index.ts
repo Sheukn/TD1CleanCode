04c6da3b46b111849f626afe6858c1ed5a9dc1ad
Steven Chouk <steven.chouk@gmail.com>
2024-11-28 12:36:10 +0100

    Quick refacto

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,18 @@
 export function calculPoints(dices : [number,number,number,number,number]) {
-    let points = 0;
     dices.sort();
-    let isStraight = true;
-    if (dices[0] === dices[4]) {
-        // YAMS
-        return 50;
-    }
+    if (dices[0] === dices[4]) return 50;
 
-    if(dices[0] === dices[3] || dices[1] === dices[4]){
-        // Carre
-        return 35;
-    }
+    if(dices[0] === dices[3] || dices[1] === dices[4]) return 35;
 
     if(dices[0] === dices[2] || dices[1] == dices[3] || dices[2] === dices[4]){
         if (dices[0] === dices[2] && dices[3] === dices[4] || 
-            dices[2] === dices[4] && dices[0] === dices[1]
-        )
+            dices[2] === dices[4] && dices[0] === dices[1])
             return 30;
         return 28;
     }
 
+    let isStraight = true;
+    let points = 0;
     for (let i = 0; i < dices.length - 1; i++){
         if(dices[i] + 1 !== dices[i + 1]){
             isStraight = false;
@@ -27,11 +20,5 @@ export function calculPoints(dices : [number,number,number,number,number]) {
         points += dices[i];
     }
 
-    points += dices[dices.length - 1];
-
-    if (isStraight) {
-        return 40;
-    }
-
-    return points;
+    return isStraight ? 40 : points += dices[4];
 }
