9270b900f44fd3b286a30efb96e907f6fb8febfb
Steven Chouk <steven.chouk@gmail.com>
2024-11-28 11:59:16 +0100

    Adding straight

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,5 +8,9 @@ describe('calculPoints', () => {
 
     it('should return 50 if dices are 1,1,1,1,1', () => {
         expect(calculPoints([1,1,1,1,1])).toBe(50);
+    });
+
+    it('should return 40 if dices are 1,2,3,4,5', () => {
+        expect(calculPoints([1,2,3,4,5])).toBe(40);
     })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 export function calculPoints(dices : [number,number,number,number,number]) {
     let points = 0;
     dices.sort();
+    let isStraight = true;
 
     if (dices[0] === dices[4]) {
         // YAMS
         return 50;
     }
-    
-    for (let i = 0; i < dices.length; i++){
+
+    for (let i = 0; i < dices.length - 1; i++){
+        if(dices[i] + 1 !== dices[i + 1]){
+            isStraight = false;
+        }
         points += dices[i];
     }
+
+    points += dices[dices.length - 1];
+
+    if (isStraight) {
+        return 40;
+    }
+
     return points;
 }
