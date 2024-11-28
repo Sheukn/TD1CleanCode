c5226279993eae1cf3ad96e2b6514cb263995175
Steven Chouk <steven.chouk@gmail.com>
2024-11-28 12:01:08 +0100

    Adding carré implementation

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ export function calculPoints(dices : [number,number,number,number,number]) {
         return 50;
     }
 
+    if(dices[0] === dices[3] || dices[1] === dices[4]){
+        return 35;
+    }
+
     for (let i = 0; i < dices.length - 1; i++){
         if(dices[i] + 1 !== dices[i + 1]){
             isStraight = false;
