f3d9202a213d77744fa812c937a3396345b276fa
Steven Chouk <steven.chouk@gmail.com>
2024-11-28 11:51:53 +0100

    Adding YAMS

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,12 @@
 import { calculPoints } from './index';
 
-it ('should return 17 if dices are 1,2,3,5,6', () => {
-    expect(calculPoints([1,2,3,5,6])).toBe(17);
+
+describe('calculPoints', () => {
+    it ('should return 17 if dices are 1,2,3,5,6', () => {
+        expect(calculPoints([1,2,3,5,6])).toBe(17);
+    });
+
+    it('should return 50 if dices are 1,1,1,1,1', () => {
+        expect(calculPoints([1,1,1,1,1])).toBe(50);
+    })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 export function calculPoints(dices : [number,number,number,number,number]) {
     let points = 0;
+    dices.sort();
+
+    if (dices[0] === dices[4]) {
+        // YAMS
+        return 50;
+    }
+    
     for (let i = 0; i < dices.length; i++){
         points += dices[i];
     }
